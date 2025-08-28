c9ef4b7d12a74ee38092357fdfadd80c4a20de3f
TehamiKamdar <tehamikamdar15@gmail.com>
2025-08-28 14:06:58 +0500

    Service pages modified

---
+++ resources/views/website/services/illustration.blade.php
@@ -0,0 +1,139 @@
+@extends('layouts.web.layout')
+@section('content')
+  <div class="service-page">
+    <!-- Hero Banner -->
+    <section class="service-hero">
+      <div class="container">
+        <nav aria-label="breadcrumb" class="service-breadcrumb">
+          <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Home</a></li>
+            <li class="breadcrumb-item"><a href="#">Services</a></li>
+            <li class="breadcrumb-item active" aria-current="page">Illustration</li>
+          </ol>
+        </nav>
+        <div class="row">
+          <div class="col-lg-8 mx-auto text-center">
+            <h1 class="display-4 fw-bold mb-4" data-aos="fade-up">Professional Illustration Services</h1>
+            <p class="lead mb-4" data-aos="fade-up" data-aos-delay="100">Lorem ipsum dolor sit amet consectetur
+              adipisicing elit. Similique dolores a voluptas maiores aliquid iste dolorem laudantium cupiditate
+              aspernatur assumenda!</p>
+            <a href="#" class="btn btn-hero-primary btn-lg px-4" data-bs-toggle="modal" data-bs-target="#exampleModal"
+              data-aos="fade-up" data-aos-delay="200">Get
+              Started Today</a>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Service Section 1 -->
+    <section class="service-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img
+              src="https://images.unsplash.com/photo-1544947950-fa07a98d237f?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+              alt="Book Writing Process" class="img-fluid service-img">
+          </div>
+          <div class="col-lg-6" data-aos="fade-left">
+            <div class="service-content">
+              <div class="service-header">
+                <div class="service-icon-box">
+                  <i class="ri-pencil-line"></i>
+                </div>
+                <h2 class="service-title">Book Writing</h2>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <button type="button" class="btn btn-hero-primary" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">Learn More <i class="ri-arrow-right-line"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Service Section 2 -->
+    <section class="service-section bg-light">
+      <div class="container">
+        <div class="row align-items-center flex-row-reverse">
+          <div class="col-lg-6" data-aos="fade-left">
+            <img
+              src="https://images.unsplash.com/photo-1455390582262-044cdead277a?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+              alt="Genre Specialization" class="img-fluid service-img">
+          </div>
+          <div class="col-lg-6" data-aos="fade-right">
+            <div class="service-content">
+              <div class="service-header">
+                <div class="service-icon-box">
+                  <i class="ri-book-open-line"></i>
+                </div>
+                <h2 class="service-title">Genre Specialization</h2>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <button type="button" class="btn btn-hero-primary" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">Learn More <i class="ri-arrow-right-line"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+
+    <!-- Service Section 3 -->
+    <section class="service-section">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-lg-6" data-aos="fade-right">
+            <img
+              src="https://images.unsplash.com/photo-1589829545856-d10d557cf95f?ixlib=rb-4.0.3&auto=format&fit=crop&w=800&q=80"
+              alt="Collaborative Process" class="img-fluid service-img">
+          </div>
+          <div class="col-lg-6" data-aos="fade-left">
+            <div class="service-content">
+              <div class="service-header">
+                <div class="service-icon-box">
+                  <i class="ri-team-line"></i>
+                </div>
+                <h2 class="service-title">Collaborative Process</h2>
+              </div>
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Eum placeat quod obcaecati eaque nihil!
+                Numquam, recusandae consequuntur! In architecto omnis accusamus eos nobis accusantium modi doloribus
+                repellendus voluptate, aut facilis.</p>
+
+              <button type="button" class="btn btn-hero-primary" data-bs-toggle="modal"
+                data-bs-target="#exampleModal">Learn More <i class="ri-arrow-right-line"></i></button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  </div>
+@endsection
